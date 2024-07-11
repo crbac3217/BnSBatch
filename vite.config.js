500cb72f434ec0d687dee1cb06bd1d756fd7ae77
crbac3217 <crbac3217@gmail.com>
2024-07-11 11:16:18 -0700

    testt

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/BnSBatch/',
+  build: {
+    outDir: 'build',  // Make sure this is correctly set
+  },
 })
